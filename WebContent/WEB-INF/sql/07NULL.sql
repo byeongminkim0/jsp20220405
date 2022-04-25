7799f447d7a843453069b86a6422fb037c0d8daf
kbm <99157638+byeongminkim0@users.noreply.github.com>
2022-04-25 16:40:22 +0900

    0425 sql(UPDATE)

--- WebContent/WEB-INF/sql/07NULL.sql
+++ WebContent/WEB-INF/sql/07NULL.sql
@@ -6,3 +6,8 @@ SELECT * FROM Employees WHERE Photo = 'pic01';
 -- 조회시 IS NULL
 SELECT * FROM Employees WHERE Photo IS NULL;
 SELECT * FROM Employees WHERE Photo IS NOT NULL;
+
+-- 그룹함수에서 보통 제외됨
+SELECT COUNT(*) FROM Employees; -- 레코드 수
+SELECT COUNT(LastName) FROM Employees;
+SELECT COUNT(Photo) FROM Employees;
--- src/chap14/javaBeans/Employee.java
+++ src/chap14/javaBeans/Employee.java
@@ -4,6 +4,9 @@ public class Employee {
 	private String firstName;
 	private String lastName;
 	private String birthDate;
+	private String photo;
+	private String notes;
+	private int id;
 
 	public String getFirstName() {
 		return firstName;
@@ -27,6 +30,30 @@ public class Employee {
 
 	public void setBirthDate(String birthDate) {
 		this.birthDate = birthDate;
+	}
+
+	public String getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+
+	public String getNotes() {
+		return notes;
+	}
+
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
 	}
 
 }
