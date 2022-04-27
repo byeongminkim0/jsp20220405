a2a7b8e6f7db195af04c022bad3bc89d85f233bc
kbm <99157638+byeongminkim0@users.noreply.github.com>
2022-04-27 15:00:26 +0900

    0427 sql

---
+++ WebContent/WEB-INF/sql/21LIKE.sql
@@ -0,0 +1,15 @@
+USE w3schools;
+
+SELECT * FROm Employees;
+
+-- LIKE : 유사한, ~같은
+-- '_' : 하나의 문자
+
+SELECT * FROM Employees WHERE Photo LIKE 'EmpID_.pic';
+
+-- '%' : 0개 이상의 문자
+
+SELECT * FROM Employees WHERE LastName LIKE 'D%';
+SELECT * FROM Employees WHERE LastName LIKE '%n';
+
+SELECT * FROM Employees WHERE LastName LIKE '%n%';
